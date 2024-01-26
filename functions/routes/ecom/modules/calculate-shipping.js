bdc1c85d8a7af7696228ddd1c439cce49bc8454a
Vitor Rocha Gonçalves <35348281+vitorrgg@users.noreply.github.com>
2024-01-26 15:01:29 -0300

    adding to total_price the additional_price when there is one

--- functions/routes/ecom/modules/calculate-shipping.js
+++ functions/routes/ecom/modules/calculate-shipping.js
@@ -282,6 +282,22 @@ exports.post = async ({ appSdk }, req, res) => {
       flags: ['correios-api']
     }
 
+    // check for default configured additional/discount price
+    if (typeof config.additional_price === 'number' && config.additional_price) {
+      if (config.additional_price > 0) {
+        shippingLine.other_additionals = [{
+          tag: 'additional_price',
+          label: 'Adicional padrão',
+          price: config.additional_price
+        }]
+      } else {
+        // negative additional price to apply discount
+        shippingLine.discount -= config.additional_price
+      }
+      // update total price
+      shippingLine.total_price += config.additional_price
+    }
+
     // search for discount by shipping rule
     if (Array.isArray(appData.shipping_rules)) {
       for (let i = 0; i < appData.shipping_rules.length; i++) {
